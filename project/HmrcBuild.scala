a7a715f5d0f0271b313dd9773fc025e39f07ba2a
petrnejedly <pnejedly@equalexperts.com>
2015-08-27 12:22:46 +0100

    Fixed project structure to follow other libraries model

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -15,97 +15,44 @@
  */
 
 import sbt.Keys._
-import sbt.Tests.{Group, SubProcess}
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object HmrcBuild extends Build {
 
-  import TestPhases._
-  import uk.gov.hmrc.DefaultBuildSettings
   import uk.gov.hmrc.DefaultBuildSettings._
+  import uk.gov.hmrc.SbtAutoBuildPlugin
+  import uk.gov.hmrc.versioning.SbtGitVersioning
 
-  val nameApp = "http-caching-client"
+  val appName = "http-caching-client"
 
-  lazy val keyStoreClient = Project(nameApp, file("."))
-    .enablePlugins(play.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning)
+  val appDependencies = Dependencies.compile
+
+  lazy val library = Project(appName, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
     .settings(
       targetJvm := "jvm-1.7",
-      libraryDependencies ++= AppDependencies(),
+      libraryDependencies ++= appDependencies,
       crossScalaVersions := Seq("2.11.7"),
-      organization := "uk.gov.hmrc"
+      resolvers := Seq(
+        Resolver.bintrayRepo("hmrc", "releases"),
+        Resolver.typesafeRepo("releases")
+      )
     )
-    .settings(inConfig(TemplateTest)(Defaults.testSettings): _*)
-    .configs(IntegrationTest)
-    .settings(inConfig(TemplateItTest)(Defaults.itSettings): _*)
-    .settings(
-      Keys.fork in IntegrationTest := false,
-      unmanagedSourceDirectories in IntegrationTest <<= (baseDirectory in IntegrationTest)(base => Seq(base / "it")),
-      addTestReportOption(IntegrationTest, "int-test-reports"),
-      testGrouping in IntegrationTest := oneForkedJvmPerTest((definedTests in IntegrationTest).value),
-      parallelExecution in IntegrationTest := false)
-    .settings(resolvers += Resolver.bintrayRepo("hmrc", "releases"))
+    .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
+
 }
 
+object Dependencies {
 
-private object TestPhases {
-
-  val allPhases = "tt->test;test->test;test->compile;compile->compile"
-  val allItPhases = "tit->it;it->it;it->compile;compile->compile"
-
-  lazy val TemplateTest = config("tt") extend Test
-  lazy val TemplateItTest = config("tit") extend IntegrationTest
-
-  def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
-    tests map {
-      test => new Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
-    }
-}
-private object AppDependencies {
-
+  import play.PlayImport._
   import play.core.PlayVersion
-
-  private val httpVerbsVersion = "1.10.0"
 
   val compile = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current % "provided",
     "uk.gov.hmrc" %% "json-encryption" % "1.9.0",
-    "uk.gov.hmrc" %% "http-verbs" % httpVerbsVersion,
+    "uk.gov.hmrc" %% "http-verbs" % "1.10.0",
     "uk.gov.hmrc" %% "play-config" % "1.2.0"
   )
 
-  trait TestDependencies {
-    lazy val scope: String = "test"
-    lazy val test : Seq[ModuleID] = ???
-  }
-
-  object Test {
-    def apply() = new TestDependencies {
-      override lazy val test = Seq(
-        "org.scalatest" %% "scalatest" % "2.2.4" % scope,
-        "org.pegdown" % "pegdown" % "1.4.2" % scope,
-        "uk.gov.hmrc" %% "http-verbs" % httpVerbsVersion % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "1.2.0" % scope
-      )
-    }.test
-  }
-
-  object IntegrationTest {
-    def apply() = new TestDependencies {
-
-      override lazy val scope: String = "it"
-
-      override lazy val test = Seq(
-        "org.scalatest" %% "scalatest" % "2.2.4" % scope,
-        "org.pegdown" % "pegdown" % "1.4.2" % scope,
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "1.2.0" % scope,
-        "uk.gov.hmrc" %% "secure" % "6.1.0" % scope
-      )
-    }.test
-  }
-
-  def apply() = compile ++ Test() ++ IntegrationTest()
 }
 
