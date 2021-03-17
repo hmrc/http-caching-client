8bdf9dcd3a59a4cf7d7feb9f5d986b82de8fec79
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-03-17 18:35:22 +0000

    BDOG-1268 Build for Play 2.8

--- src/test/scala/uk/gov/hmrc/http/cache/client/HttpCachingClientSpec.scala
+++ src/test/scala/uk/gov/hmrc/http/cache/client/HttpCachingClientSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 import play.api.libs.json._
 import uk.gov.hmrc.http._
-import uk.gov.hmrc.http.logging.SessionId
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
